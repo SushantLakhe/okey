d1141c79d4fdbb99a641baba66a1b44f90c4358d
SushantLakhe <s68352467@gmail.com>
2025-12-26 13:35:07 +0545

    animal is added

--- payment.java
+++ payment.java
@@ -4,4 +4,7 @@ public class payment {
 	public void money(){
 		System.out.println("give me my money");
 	}
+	public void anyKind() {
+		System.out.println("animal");
+	}
 }
--- task2.java
+++ task2.java
@@ -5,5 +5,9 @@ public class task2 {
 	public void anymethod() {
 			System.out.println("okey");
 		}
+	
+	public void anyKind() {
+		System.out.println("animal");
+	}
 		
 }
